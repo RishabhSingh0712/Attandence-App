4f0d4f88e96b4e54bd6f6f66772ef321bdd62d0c
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-01-27 15:35:02 +0530

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,21 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import cors from 'cors';
-import { Register } from './model/Register.js';
+import  Register from './model/Register.js';
+import userRoutes from './routes/userRoutes.js'
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/AttendanceApp");
+mongoose.connect("mongodb+srv://srishabhdev07:Rishabh123@cluster1.az5qugh.mongodb.net");
+mongoose.connection.on("error", (err) => {
+    console.error("MongoDB connection error:", err);
+  });
+  
 
 app.post('/register',(req,res)=>{
+    console.log('data received');
 Register.create(req.body)
 .then(Register=>res.json(Register))
 .catch(err=>res.json(err))
