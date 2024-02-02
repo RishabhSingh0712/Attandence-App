3b24aa9289c50c2ecc5c802a12c7491774c242fd
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-02-02 12:26:06 +0530

    update

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,100 +1,83 @@
-import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const token = localStorage.getItem("jwtToken");
 
-    // const token = localStorage.getItem("jwtToken");
-
-    // const handleLogout = async() => {
-    //     try {
-    //       if (response.status === 200) {
-    //         localStorage.removeItem("jwtToken");
-    //         console.log("Logout successful");
-    //         setIsLoggedIn(false);
-    //         navigate("/");
-    //       } else {
-    //         console.log("Logout failed");
-    //       }
-    //     } catch (error) {
-    //       console.error("Error during logout:", error);
-    //     }    
-    //   };
-
-    const token = localStorage.getItem("jwtToken");
-
-const handleLogout = async () => {
+  const handleLogout = async () => {
     try {
-        const response = await fetch("http://127.0.0.1:5000/api/user/logout", {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json",
-            },
-        });
+      const response = await fetch("http://127.0.0.1:5000/api/user/logout", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
 
-        if (response.status === 200) {
-            localStorage.removeItem("jwtToken");
-            console.log("Logout successful");
-            setIsLoggedIn(false);
-            navigate("/");
-        } else {
-            console.log("Logout failed");
-        }
+      if (response.status === 200) {
+        localStorage.removeItem("jwtToken");
+        console.log("Logout successful");
+        setIsLoggedIn(false);
+        navigate("/");
+      } else {
+        console.log("Logout failed");
+      }
     } catch (error) {
-        console.error("Error during logout:", error);
+      console.error("Error during logout:", error);
     }
-};
+  };
 
-    return (
-        <header className="shadow sticky z-50 top-0">
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
-                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <Link to="/" className="flex items-center">
-                        <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                            className="mr-3 h-12"
-                            alt="Logo"
-                        />
-                    </Link>
-                    <div className="flex items-center lg:order-2">
-                        {isLoggedIn ? true(
-                            <NavLink
-                                onClick={handleLogout}
-                                className="text-white bg-blue-500 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                                activeClassName="text-orange-700"
-                            >
-                                Logout
-                            </NavLink>
-                        ) : (
-                            <NavLink
-                                to="/login"
-                                className="text-white bg-blue-500 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                                activeClassName="text-orange-700"
-                            >
-                                Login
-                            </NavLink>
-                        )}
-                    </div>
-                    <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-                        id="mobile-menu-2"
-                    >
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
-                                <NavLink
-                                    to="/"
-                                    activeClassName="text-orange-700"
-                                    className="block py-2 pr-4 pl-3 duration-200
-                                        text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0"
-                                >
-                                    Home
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="shadow sticky z-50 top-0">
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <Link to="/" className="flex items-center">
+            <img
+              src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+              className="mr-3 h-12"
+              alt="Logo"
+            />
+          </Link>
+          <div className="flex items-center lg:order-2">
+            {isLoggedIn ? (
+              <NavLink
+                onClick={handleLogout}
+                className="text-white bg-blue-500 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                activeClassName="text-orange-700"
+              >
+                Logout
+              </NavLink>
+            ) : (
+              <NavLink
+                to="/login"
+                className="text-white bg-blue-500 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                activeClassName="text-orange-700"
+              >
+                Login
+              </NavLink>
+            )}
+          </div>
+
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <NavLink
+                  to="/"
+                  activeClassName="text-orange-700"
+                  className="block py-2 pr-4 pl-3 duration-200
+                 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0"
+                >
+                  Home
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
