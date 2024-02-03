6061f5cdca78eea6c88803e6b70ceaf416d80fa2
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-02-03 10:57:33 +0530

    Update login

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -15,7 +15,6 @@ const Login = () => {
     password: "",
   });
 
-
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -53,20 +52,23 @@ const Login = () => {
         axios
           .post("http://127.0.0.1:5000/api/user/login", data)
           .then((response) => {
-            if (response.status === 400) {
-              console.log("User already present");
-              setIsLoggedIn(true);
+            if (response.status === 201) { 
               
-            } else {
               alert("Congratulations, login successful!!");
+              window.localStorage.setItem("token",response.data['token']);
+              const userInfoString = JSON.stringify(response.data['user_info']);
+              window.localStorage.setItem("user_info",userInfoString);
               navigate("/");
+            } else {
+              
+              alert("Error, login failed!!");
             }
           })
           .catch((error) => {
-            alert("User not found or password incorrect")
+            alert("User not found or password incorrect");
           });
       } catch (error) {
-        alert("server not responding please try again later")
+        alert("server not responding please try again later");
       }
     } else {
       setErrors(newErrors);
@@ -76,66 +78,66 @@ const Login = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="max-w-md w-full p-6 bg-white rounded-md shadow-md">
-        <h2 className="text-3xl font-semibold text-center mb-6">Employee Login</h2>
-          <div>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className={`mt-1 p-2 w-full border rounded-md ${
-                  errors.email ? "border-red-500" : "border-gray-300"
-                }`}
-              />
-              {errors.email && (
-                <p className="text-red-500 text-sm mt-1">{errors.email}</p>
-              )}
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                className={`mt-1 p-2 w-full border rounded-md ${
-                  errors.password ? "border-red-500" : "border-gray-300"
-                }`}
-              />
-              {errors.password && (
-                <p className="text-red-500 text-sm mt-1">{errors.password}</p>
-              )}
-            </div>
-            <button
-              type="submit"
-              className="bg-blue-500 text-white p-2 rounded-md w-full hover:bg-blue-600"
+        <h2 className="text-3xl font-semibold text-center mb-6">
+          Employee Login
+        </h2>
+        <div></div>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-600"
             >
-              Login
-            </button>
-            <p className="text-center mt-4">
-              New Employee?{" "}
-              <Link to="/Registerpage" className="text-blue-500">
-                Register Here
-              </Link>
-            </p>
-          </form>
-        
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className={`mt-1 p-2 w-full border rounded-md ${
+                errors.email ? "border-red-500" : "border-gray-300"
+              }`}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className={`mt-1 p-2 w-full border rounded-md ${
+                errors.password ? "border-red-500" : "border-gray-300"
+              }`}
+            />
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white p-2 rounded-md w-full hover:bg-blue-600"
+          >
+            Login
+          </button>
+          <p className="text-center mt-4">
+            New Employee?{" "}
+            <Link to="/Registerpage" className="text-blue-500">
+              Register Here
+            </Link>
+          </p>
+        </form>
       </div>
     </div>
   );
