e60f426a8e8de0105b8147e96ad7b1eac6bebcac
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-02-06 16:12:19 +0530

    Update

--- Backend/model/userModel.js
+++ Backend/model/userModel.js
@@ -1,4 +1,4 @@
-import {model,Schema} from "mongoose";
+import { model, Schema } from "mongoose";
 
 const attendanceSchema = new Schema({
   checkInTime: {
@@ -14,36 +14,35 @@ const attendanceSchema = new Schema({
     default: null,
   },
 });
-const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: { 
-      type: Number,
-      required: true,
-      unique: true,
-    },
-    Email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-    },
-    Password: {
-        type: String,
-        required: [true, "Password is required"],
-    },
-    ConfirmPassword: {
-        type: String,
-        required: [true, "Password is required"],
-    },
-    attendance: [attendanceSchema]
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
   },
-);
-
+  phoneNumber: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  Password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  ConfirmPassword: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  attendance: [attendanceSchema],
+});
 
 const User = model("user", userSchema);
 export default User;
