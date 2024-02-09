b3c799c693900e9ec787d9acbd13b035c674c9c3
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-02-09 11:54:58 +0530

    Update

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { register, login,Attendance} from "../Controller/authController.js";
+import { register, login,attendance} from "../Controller/authController.js";
 
 
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/Attendance", Attendance)
+router.post("/attendance", attendance)
 
 export default router;
 
