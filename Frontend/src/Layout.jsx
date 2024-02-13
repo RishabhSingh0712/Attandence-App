ba17c3766496434f656215e124bb9e563d9cfe4a
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-02-13 17:58:46 +0530

    Update

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+// import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
@@ -8,7 +8,7 @@ function Layout() {
     <div>
       <Header />
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
