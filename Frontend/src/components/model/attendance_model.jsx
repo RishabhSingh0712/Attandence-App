431722d7c153d1eb4c1efa532213f5610a636bd3
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-02-18 20:39:53 +0530

    update

---
+++ Frontend/src/components/model/attendance_model.jsx
@@ -0,0 +1,15 @@
+// userModel.js
+
+const AttendanceModel = {
+    id: null,
+    name: '',
+    email: '',
+    date:null,
+    checkInTime:null,
+    checkOutTime:null,
+    workingHours:0,
+    type:''
+  };
+  
+  export default AttendanceModel;
+  
