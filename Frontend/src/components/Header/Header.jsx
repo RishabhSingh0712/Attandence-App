39bd046954d90838590a2f0e4e3cfe886378833d
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-03-12 16:48:47 +0530

    Update

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -36,18 +36,18 @@ export default function Header() {
     <header className="shadow sticky z-50 top-0">
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link to="/" className="flex items-center ">
+          <Link to="/" className="flex items-center sm:w-60 w-36">
             <img
               src="https://mizzlecodes.com/wp-content/uploads/2022/06/logo-sq-rctngl-clr-nblu-20.png"
               className="mr-7 h-8 "
               alt="Logo"
             />
           </Link>
-          <div className="flex items-center lg:order-2">
+          <div className="flex items-center lg:order-2 ">
             {isLoggedIn ? (
               <NavLink
                 onClick={handleLogout}
-                className="text-white bg-blue-500 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                className="text-white bg-blue-500 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none "
               >
                 Logout
               </NavLink>
