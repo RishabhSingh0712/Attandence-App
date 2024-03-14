7400593343c0ad1b003c27d5440b2fc3cd6b51a1
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-03-14 11:25:54 +0530

    delete toast method

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+
+
 const Login = () => {
   const navigate = useNavigate();
 
