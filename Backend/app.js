f1b82c54ad7e4f4aa5b97e24a789e2d8596744a2
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-03-29 15:17:10 +0530

    update

---
+++ Backend/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import connectDB from './databse/db.js';
+import userRoutes from './routes/userRoutes.js';
+import cors from 'cors';
+import 'dotenv/config.js';
+
+
+const app = express();
+const PORT = process.env.PORT || 6000;
+
+//middleware
+app.use(express.json());
+
+
+app.use(cors());
+
+// define routes 
+app.use('/api/user', userRoutes);
+
+//database call
+connectDB();
+
+app.listen(PORT,()=>{
+console.log(`Server on listing http://127.0.0.1:${PORT}`);
+})
+
+
